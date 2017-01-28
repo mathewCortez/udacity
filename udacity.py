d9511d39af8ffcdab4de84fe803e8518f00cf9ac
Mathew Cortez <mcortez121@gmail.com>
2017-01-27 19:15:29 -0800

    Update: udacity.py
    Added in a function that finds the longest palindrome within a string
    and returns the palindrome.

--- udacity.py
+++ udacity.py
@@ -12,9 +12,46 @@ def question1(s, t):
         
     return True # is anagram
 
-def question2(a)
 
-    return string
+def question2(a):
+    end = 1 # holds value for longest palindrome / for string splicing with start
+    start = 0 # when it will start / again needed for splicing
+    length = len(a)
+    low = 0 #low and high are use to compare string characters
+    high = 0
+    for i in xrange(1, length): # xrange to save memory although its not a big memory savor 
+        # test each spot as the center of the string / for even palindrome
+        low = i - 1 
+        high = i
+        # if the two consecutive characters are the same then we could have the beginning of a palindrome
+        while low >= 0 and high < length and a[low] == a[high]:
+            if high - low + 1 > end:
+                start = low # set new starting point for longest palindrome
+                end = high - low + 1 # set new longest length
+                
+            low -= 1 # move low down and high up to see if the palindrome continues
+            high += 1
+        # same as code above but for when the palindrome is odd like racecar 
+        low = i - 1 # one below palindrome center
+        high = i + 1 # one above
+        while low >= 0 and high < length and a[low] == a[high]:
+            if high - low + 1 > end:
+                start = low
+                end = high - low + 1
+            low -= 1
+            high += 1
+ 
+    return a[start:start + end]
+
+
+
+
 
 
 print(question1('udacity','ud'))
+
+
+print(question2(alracecaral))
+ 
+
+
