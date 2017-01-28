58757225d08a8deeddd9a1d2d661f13c9b48f324
Mathew Cortez <mcortez121@gmail.com>
2017-01-28 13:50:26 -0800

    Update: udacity.pu
    I added in a statement to catch an empty list in question2 so when an
    empty list is taken in, it will just return an empty list.  Also added
    file to test the third question.

--- udacity.py
+++ udacity.py
@@ -14,6 +14,8 @@ def question1(s, t):
 
 
 def question2(a):
+    if a == '': # if an empty string is entered I return the empty list before declaring variables
+        return ""
     end = 1 # holds value for longest palindrome / for string splicing with start
     start = 0 # when it will start / again needed for splicing
     length = len(a)
@@ -43,15 +45,113 @@ def question2(a):
  
     return a[start:start + end]
 
+'''class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def search(self, find_val):
+        return self.preorder_search(tree.root, find_val)
+
+    def print_tree(self):
+        return self.preorder_print(tree.root, "")[:-1]
+
+    def preorder_search(self, start, find_val):
+        if start:
+            if start.value == find_val:
+                return True
+            else:
+                return self.preorder_search(start.left, find_val) or self.preorder_search(start.right, find_val)
+        return False
+
+    def preorder_print(self, start, traversal):
+        if start:
+            traversal += (str(start.value) + "-")
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+        return traversal'''
+    
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
 
+class BST(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def insert(self, new_val):
+        self.insert_helper(self.root, new_val)
+
+    def insert_helper(self, current, new_val):
+        if current.value < new_val:
+            if current.right:
+                self.insert_helper(current.right, new_val)
+            else:
+                current.right = Node(new_val)
+        else:
+            if current.left:
+                self.insert_helper(current.left, new_val)
+            else:
+                current.left = Node(new_val)
+
+    def search(self, find_val):
+        return self.search_helper(self.root, find_val)
+
+    def search_helper(self, current, find_val):
+        if current:
+            if current.value == find_val:
+                return True
+            elif current.value < find_val:
+                return self.search_helper(current.right, find_val)
+            else:
+                return self.search_helper(current.left, find_val)
+        return False
+# Set up tree
+tree = BST(4)
+
+# Insert elements
+tree.insert(2)
+tree.insert(1)
+tree.insert(3)
+tree.insert(5)
+
+# Check search
+# Should be True
+print tree.search(4)
+# Should be False
+print tree.search(6)
+
+'''# Set up tree
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+
+# Test search
+# Should be True
+print tree.search(4)
+# Should be False
+print tree.search(6)
+
+# Test print_tree
+# Should be 1-2-4-5-3
+print tree.print_tree()'''
 
 
 
 print(question1('udacity','ud'))
 
 
-print(question2(alracecaral))
- 
+print(question2("alracecaral"))
+
 
 
